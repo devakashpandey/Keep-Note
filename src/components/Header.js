336a85eaef2a17cbc2942d640e2baf2243a4b7d1
DevAkky <developerakky@gmail.com>
2023-03-18 10:53:01 +0530

    header done

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Header.css";
+import img from "../asset/icon2.png";
+
+const Header = () => {
+  return (
+    <>
+      <div class="header">
+        <img src={img} alt="img" height="45px" width="50px"></img>
+        <h1 className="keep-head">Keep Notes</h1>
+      </div>
+    </>
+  );
+};
+
+export default Header;
